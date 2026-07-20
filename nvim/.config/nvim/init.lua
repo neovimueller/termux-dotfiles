85139a0312f25bf3e5532463f825b07ca3569036
ismael mueller <ismoliveira@inf.ufrgs.br>
2026-07-19 23:43:28 -0300

    update nvim config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,6 +1,20 @@
-vim.cmd([[highlight Normal ctermbg=black guibg=black]])
-vim.opt.expandtab = true
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.cmd("syntax on")
+-- basics
+vim.opt.termguicolors = true
+vim.cmd('syntax enable') -- syntax highlighting only, no colorscheme
 vim.cmd("filetype plugin indent on")
+vim.cmd("highlight Normal ctermbg=black guibg=black")
+
+-- line numbers
+vim.opt.number = true          -- show current line number
+vim.opt.relativenumber = true  -- show distance to other lines
+
+-- tabs - PEP8
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.smartindent = true
+
+-- python auto-indent
+vim.opt.filetype.plugin = true
+vim.opt.filetype.indent = true
